68247a93fa9fa50ea00ee0ec594553a8fac4768c
Ray Yang <k1862145@gmail.com>
2022-06-27 00:21:39 +0800

    Update

--- Chapter 2-Operators,Truthy and Falsy Values.py
+++ Chapter 2-Operators,Truthy and Falsy Values.py
@@ -1,15 +1,46 @@
-#Comparison and Assignment Operators
+# Comparison and Assignment Operators
+from audioop import reverse
 
 
+a = 10
+b = 20
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
 
-#Logical and Bitwise Operators
+c = "today is a good day."
+print(c == c.lower())
 
 
+def math(index, a, b):
+    if index == 1:
+        a += b
+    elif index == 2:
+        a -= b
+    elif index == 3:
+        a *= b
+    elif index == 4:
+        a /= b
+    else:
+        a **= b
+    print(a)
 
-#Truthy and Falsy Values
+
+a = 10
+b = 20
+math(1, a, b)
+math(2, a, b)
+math(3, a, b)
+math(4, a, b)
+math(5, a, b)
 
 
+# Logical and Bitwise Operators
+
+# Truthy and Falsy Values
 
 
-#Short-Circuit Evaluation
- 
+# Short-Circuit Evaluation
--- Chapter 3-Python Control Flow.py
+++ Chapter 3-Python Control Flow.py
@@ -1,17 +1,45 @@
-#If Statement
-if True:
-    print("This is so true!")
-else:
-    print("This is false.")
-
-
-age = 15
-if age < 8:
-    print("Movie is free for you!!")
-elif (8 <= age < 65):
-    print("you need to pay 300!")
-else:
-    print("You only need to pay $150")
-
-#program ask user's name
+# program ask user's name
+# program checks if the user has more than or equal to $30
 name = input("Enter your name: ")
+money = input("Enetr your cash amount: ")
+hungry = input("Are you hungry? (Y/N) ")
+
+if hungry == "Y":
+    if int(money) >= 30:
+        print(f"{name} should go to eat breakfast.")
+    else:
+        print(f"{name} is hungry but might not have enough money to buy breakfast.")
+elif hungry == "N":
+    if int(money) >= 30:
+        print(f"{name} has bought but doesn't want to eat breakfast.")
+    else:
+        print(f"{name} has no money but is not hungry..")
+else:
+    print("Please make sure that you enter either Y or N.")
+
+# For and While Loop
+# for variable in iterable:
+#     do something here
+for letter in "Hello World":
+    if (letter == letter.upper()):
+        print(letter)
+
+myList = [1, 3, 5, 7, 9]
+for num in myList:
+    print(num)
+
+# a list of tuples
+for tuple in [(1, 2), (3, 5), (5, 7)]:
+    print(tuple)
+
+for a, b in [(1, 2), (3, 5), (5, 7)]:
+    print(a + b)
+
+# ditionary (key)
+myDictionary = {"name": "Ray", "age": 27}
+for key, value in myDictionary.items():
+    print(f"The key is {key}")
+    print(f"The value is {value}")
+
+
+# While loop
